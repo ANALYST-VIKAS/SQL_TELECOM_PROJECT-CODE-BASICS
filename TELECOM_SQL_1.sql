dcb892dfdb48e983559e2ffdc0c74f1b1f543bde
Vikas Yadav <152646961+ANALYST-VIKAS@users.noreply.github.com>
2024-03-10 14:52:48 +0530

    Update TELECOM_SQL_1.sql

--- TELECOM_SQL_1.sql
+++ TELECOM_SQL_1.sql
@@ -1,18 +1,18 @@
 -- REVENUE BY CITY BEFORE/AFTER 5G
-SELECT city_name,
+SELECT  city_name,
 	ROUND(SUM(CASE WHEN before_after_5g="Before 5G" THEN atliqo_revenue_crores ELSE 0 END),2) AS Revenue_before_5G,
 	ROUND(SUM(CASE WHEN before_after_5g="After 5G" THEN atliqo_revenue_crores ELSE 0 END),2) AS Revenue_After_5G
     FROM (
 	SELECT D.city_name,before_after_5g,atliqo_revenue_crores
-    FROM fact_atliqo_metrics F JOIN
-    dim_cities D ON D.city_code=F.city_code JOIN
-    dim_date  A ON A.date=F.date
-    ) T
+        FROM fact_atliqo_metrics F JOIN
+        dim_cities D ON D.city_code=F.city_code JOIN
+        dim_date  A ON A.date=F.date
+         ) T
     GROUP BY 1;
     
 -- PERCENT GROWTH IN REVENUE AFTER 5G BY CITY
 	WITH CTE AS(
-    SELECT city_name,
+        SELECT city_name,
 	ROUND(SUM(CASE WHEN before_after_5g="Before 5G" THEN atliqo_revenue_crores ELSE 0 END),2)AS Revenue_before_5G,
 	ROUND(SUM(CASE WHEN before_after_5g="After 5G" THEN atliqo_revenue_crores ELSE 0 END),2)AS Revenue_After_5G
 		FROM (
@@ -39,24 +39,24 @@ SELECT city_name,
     dim_cities C ON C. city_code=F.city_code
     ) T
     GROUP BY 1
-				)
-	SELECT city_name,Users_before_5G,Users_after_5G,ROUND((Users_after_5G/Users_before_5G-1)*100,1) as users_pct_growth 
-	FROM CTE
+	)
+    SELECT city_name,Users_before_5G,Users_after_5G,ROUND((Users_after_5G/Users_before_5G-1)*100,1) as users_pct_growth 
+    FROM CTE
     ORDER BY 4 DESC ;
 
--- CHURN RATE
-SELECT D.month_name,Active_customers,Customer_lost,
-	   CONCAT(ROUND(Customer_lost/Active_Customers *100,1),"%") AS Churn_rate
-FROM (
-SELECT month_name,ROUND(sum(active_users_lakhs),1) AS Active_customers,
-				  ROUND(sum(unsubscribed_users_lakhs),1) AS Customer_lost
-FROM fact_atliqo_metrics F JOIN
-dim_cities C ON C.city_code=F.city_code JOIN
-dim_date D ON D.date=F.date
-GROUP BY 1
-     ) T JOIN
-dim_date D ON D.month_name=T.month_name
-order by MONTH(D.date)
+-- CHURN RATE CHURN RATE =(CUSTOMER LOST/ACTIVE CUSTOMERS IN A GIVEN PERIOD)*100
+	SELECT D.month_name,Active_customers,Customer_lost,
+		   CONCAT(ROUND(Customer_lost/Active_Customers *100,1),"%") AS Churn_rate
+	FROM (
+	SELECT month_name,ROUND(sum(active_users_lakhs),1) AS Active_customers,
+					  ROUND(sum(unsubscribed_users_lakhs),1) AS Customer_lost
+	FROM fact_atliqo_metrics F JOIN
+	dim_cities C ON C.city_code=F.city_code JOIN
+	dim_date D ON D.date=F.date
+	GROUP BY 1
+	     ) T JOIN
+	dim_date D ON D.month_name=T.month_name
+	order by MONTH(D.date);
 
 
 
